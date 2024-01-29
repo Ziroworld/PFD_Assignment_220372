3400d26f02aaff9456a97c866ec97bb566e949fc
Rohan manandhaar <118821550+Ziroworld@users.noreply.github.com>
2024-01-30 01:56:00 +0545

    chore: Set up initial project structure and files for assignment task
    Initialized the project directory with essential structure and base files, laying the groundwork for the upcoming assignment tasks.

---
+++ Assignment/src/main/java/com/mycompany/assignment/Assignment.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.assignment;
+
+/**
+ *
+ * @author rohan-manandhar
+ */
+public class Assignment {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
